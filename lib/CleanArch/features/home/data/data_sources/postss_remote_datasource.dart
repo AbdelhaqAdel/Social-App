161d544ba4e70625f638292d4906b82831b8d58a
abdelhaq <147981005+AbdelhaqAdel@users.noreply.github.com>
2024-09-06 20:00:02 +0300

    add snapshot to posts screen

--- lib/CleanArch/features/home/data/data_sources/postss_remote_datasource.dart
+++ lib/CleanArch/features/home/data/data_sources/postss_remote_datasource.dart
@@ -1,29 +1,76 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:newapp/CleanArch/features/home/data/models/post_model.dart';
 
 
 abstract class PostRmoteDataSourse{
-  Future <List<PostModel>>fetchPostss();
+  Future <List<PostModel>>fetchPosts();
 }
 class PostRemoteDatasourceImpl extends PostRmoteDataSourse{
-  List<PostModel>coursesList=[];
+  List<PostModel>postsList=[];
   @override
-  Future<List<PostModel>> fetchPostss()async {
-    // TODO: implement fetchPostss
-    throw UnimplementedError();
-
-// //      await  DioHelper.get(
-// //         url: EndPoint.allCourses,
-// //         token: 'eyJhbGciOiJodHRwOi8vd3d3LnczLm9yZy8yMDAxLzA0L3htbGRzaWctbW9yZSNobWFjLXNoYTI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9naXZlbm5hbWUiOiJhYmR1bG1hamVlZHNhbGxhbSIsImh0dHA6Ly9zY2hlbWFzLnhtbHNvYXAub3JnL3dzLzIwMDUvMDUvaWRlbnRpdHkvY2xhaW1zL2VtYWlsYWRkcmVzcyI6IkFiZHVsTWFqZWVkU2FsbGFtQGdtYWlsLmNvbSIsImh0dHA6Ly9zY2hlbWFzLm1pY3Jvc29mdC5jb20vd3MvMjAwOC8wNi9pZGVudGl0eS9jbGFpbXMvcm9sZSI6IlN0dWRlbnQiLCJleHAiOjE3MjU2NTMzMzEsImlzcyI6Imh0dHBzOi8vbG9jYWxob3N0OjcyODYiLCJhdWQiOiJNeVNlY3VyZWRBcGlVc2VycyJ9.wWU3aYuyclcRU_lJM9GF54fndMSATcQdp4KfBrsk_NQ',
-// //       ).then((value) {
-// //        fillCoursesList(value);
-// //   });
-// //     HiveService.saveDataToHive<CoursesModel>(HiveConstants.coursesBox,coursesList, HiveConstants.coursesBox);
-// //    return coursesList;
-// // }
-// //  void fillCoursesList(Response<dynamic> list) {
-// //      for (var element in list.data) {
-// //      coursesList.add(CoursesModel.fromJson(element));
-// //     }
-// //   }
+  Future<List<PostModel>> fetchPosts()async {
+      FirebaseFirestore.instance.collection('posts').orderBy('postDate')
+      .snapshots().listen((value) {
+        fillPostsList(value.docs);
+      });
+    // HiveServices.saveDataToHive<PostModel>(HiveConstants.postBox,postsList, HiveConstants.postBox);
+   return postsList;
 }
+
+ void fillPostsList(list) {
+      postsList=[];
+     for (var element in list){
+        // if(postsList.length < list.length){
+          postsList.add(PostModel.fromJson(element.data()));
+       }
+    //  }
+  //  }}
+  //  else if(postsList.length<list.length){
+  //     int newPostsNum=list.length-postsList.length;
+  //      for (var element in list[newPostsNum]) {
+  //        if(postsList.length<)
+  //      postsList.add(PostModel.fromJson(element.data()));
+  //     for(int i=0;i<newPostsNum;i++){
+  //       postsList.add(PostModel.fromJson())
+  //     }
+  //   }
+ 
+ 
+ if(postsList.length < list.length){
+ for (int i = postsList.length; i < list.length; i++) {
+    postsList.add(PostModel.fromJson(list[i].data()));
+ }  
+   }
+ }
+    // If the index exists in postsList, update the item
+  //   if (i < postsList.length) {
+  //     postsList[i] = postData;
+  //   } else {
+  //     // Otherwise, add the new item to the list
+  //     postsList.add(postData);
+  //   }
+  // }
+  // }
+
+
+  
+  // void GetAllPosts(){
+
+  //   FirebaseFiarestore
+  //       .instance.collection('posts').orderBy('postDate')
+  //   //.orderBy('postDate',descending: true)
+  //       .snapshots().listen((value) {//value is all collections in post  (all posts)
+  //     posts=[];
+  //     likesNum=[];
+  //     likes=[];
+  //     value.docs.forEach((element) {//element is map<String dynamic> contain all data in post model
+  //       posts.add(PostModel.fromJson(element.data()));
+  //       // changeLikeButtonColor(element.id);
+  //       // print(isLiked);
+  //     });
+  //     // posts.forEach((element) {
+  //     //   print('posts text ${element.postText}');
+  //     // });
+  //   });
+  // }
 }
--- lib/CleanArch/features/home/data/repositories/posts_repo_impl.dart
+++ lib/CleanArch/features/home/data/repositories/posts_repo_impl.dart
@@ -1,5 +1,8 @@
 import 'package:dartz/dartz.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:newapp/CleanArch/core/utils/error_handliing.dart';
+import 'package:newapp/CleanArch/core/utils/firebase_handle_error.dart';
+import 'package:newapp/CleanArch/features/home/data/data_sources/post_data_remote_datasource.dart';
 import 'package:newapp/CleanArch/features/home/data/data_sources/posts_local_datasource.dart';
 import 'package:newapp/CleanArch/features/home/data/data_sources/postss_remote_datasource.dart';
 import 'package:newapp/CleanArch/features/home/data/models/post_model.dart';
@@ -8,30 +11,25 @@ import 'package:newapp/CleanArch/features/home/domain/repositories/post.dart';
 
 class PostRepository extends PostRepo{
   final PostRemoteDatasourceImpl postRemoteDataSource;
+  final PostDataRemoteDatasourceImpl postData;
   final PostLocalDatasourceImpl postLocalDatasource;
-  PostRepository({required this.postRemoteDataSource,required this.postLocalDatasource});
+  PostRepository({required this.postRemoteDataSource,required this.postLocalDatasource,required this.postData});
+  List<String>postLikes=[];
+  List<int>postLikeNum=[];
+  List<int>postCommrntsNum=[];
   @override
-  // Future<Either<Failure, List<CoursesModel>>> getAllCourses() async{
-  // try{
-  //   //  List<CoursesModel>cachedCourses = await coursesLocalDatasource
-  //   //      .fetchCoursesFromHive();
-  //   //  if(cachedCourses.isNotEmpty){
-  //   //    return right(cachedCourses);
-  //   //  }
-  //  List<CoursesModel>courses=await coursesDataSource.fetchCourses();
-  //   return right(courses);
-  // }catch(error){
-  //   if(error is DioException){
-  //     return left(ServerFailure.fromDiorError(error));
-  //   }else{
-  //     return left(ServerFailure(error.toString()));
-  //   }
-  //  }
-  // }
-
-  @override
-  Future<Either<Failure, List<PostModel>>> getAllPosts() {
-    // TODO: implement getAllPosts
-    throw UnimplementedError();
+  Future<Either<Failure, List<PostModel>>> getAllPosts()async {
+    try{
+      final List<PostModel>posts=await postRemoteDataSource.fetchPosts();
+      postData.fetchPostData().then((value){
+        postLikes=postData.postLikes;
+        postLikeNum=postData.postLikeNum;
+        postCommrntsNum=postData.postCommrntsNum;
+      });
+      return right(posts);
+    }catch(e){
+      if(e is FirebaseAuthException){return left(FirebaseError.firebaseException(e));}
+      else{return left(ServerFailure(e.toString()));}
+    }
   }
 }
