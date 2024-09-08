83f0b2310feb7de50da658ae6731c4bf6ae2d007
abdelhaq <147981005+AbdelhaqAdel@users.noreply.github.com>
2024-09-08 18:00:17 +0300

    refactor posts cubit

--- lib/CleanArch/features/home/presentation/manager/cubit/post_cubit.dart
+++ lib/CleanArch/features/home/presentation/manager/cubit/post_cubit.dart
@@ -1,14 +1,18 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:newapp/CleanArch/features/home/data/models/post_model.dart';
+import 'package:newapp/CleanArch/features/home/data/repositories/post_reaction_reo_impl.dart';
 import 'package:newapp/CleanArch/features/home/data/repositories/posts_repo_impl.dart';
 
 part 'post_state.dart';
 
 class PostCubit extends Cubit<PostState> {
-  PostCubit({required this.postRepository}) : super(PostInitial());
+  PostCubit({required this.postRepository,required this.postReactRepo}) : super(PostInitial());
   final PostRepository postRepository;
+  final PostReactoinRepository postReactRepo;
   static PostCubit get(context)=>BlocProvider.of(context);
- 
+  
+  List <String>postsId=[];
   List<PostModel>allPostsList=[];
   Future <void>fetchAllPosts()async{
     final response=await postRepository.getAllPosts();
@@ -22,5 +26,32 @@ class PostCubit extends Cubit<PostState> {
         postLikeNum: postRepository.postLikeNum,
         postCommrntsNum: postRepository.postCommrntsNum));
      });
+    getPostsId();
+  
   }  
+
+
+  void addLike({required int postIndex}){
+     postReactRepo.addLike(postsId[postIndex]);
+  }
+
+  Future<void> getPostsId()async {
+     FirebaseFirestore.instance.collection('posts').orderBy('postDate')
+      .snapshots().listen((value) {
+        fillPostsIdList(value.docs);
+      });
+  }
+   void fillPostsIdList(list) {
+      postsId=[];
+     for (var element in list){
+          postsId.add(element.id);
+       }
+ }
+           
+
+
 }
+
+
+
+
