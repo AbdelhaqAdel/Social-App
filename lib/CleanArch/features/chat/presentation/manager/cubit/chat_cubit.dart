9652237c1bb83e0254e0210f57410d14d962b22a
abdelhaq <147981005+AbdelhaqAdel@users.noreply.github.com>
2024-09-24 07:04:02 +0300

    refactor chat cubit

--- lib/CleanArch/features/chat/presentation/manager/cubit/chat_cubit.dart
+++ lib/CleanArch/features/chat/presentation/manager/cubit/chat_cubit.dart
@@ -1,11 +1,57 @@
 import 'package:bloc/bloc.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:newapp/CleanArch/features/chat/data/models/messages_model.dart';
+import 'package:newapp/CleanArch/features/chat/domain/use_cases/add_message_use_case.dart';
+import 'package:newapp/CleanArch/features/chat/domain/use_cases/get_all_users_use_case.dart';
+import 'package:newapp/CleanArch/features/chat/domain/use_cases/get_messages_use_case.dart';
 import 'package:meta/meta.dart';
-
+import 'package:newapp/CleanArch/features/profile/data/models/user_model.dart';
 part 'chat_state.dart';
 
 class ChatCubit extends Cubit<ChatState> {
-  ChatCubit() : super(ChatInitial());
+  static ChatCubit get(context)=>BlocProvider.of(context);
+  final AddMessageUseCase addMessageUseCase;
+  final GetMessagesUseCase getMessagesUseCase;
+  final GetAllUsersUseCase getAllUsersUseCase;
 
+  ChatCubit({
+    required this.addMessageUseCase,
+    required this.getMessagesUseCase,
+    required this.getAllUsersUseCase  
+  }) : super(ChatInitial());
 
- 
+  Future<void> addToUserChat({
+    required String receiver,
+    required String message,
+  }) async {
+
+    emit(AddChatLoadingState());
+      final result= await addMessageUseCase.call(
+         receiver, message,
+      );
+      result.fold((l) => emit(AddChatErrorState()),
+          (r) => emit(AddChatSuccessState()));
+    }
+
+  List<MessagesModel>messages=[];
+  void getMessages({required String userId}) {
+    emit(GetAllMessagesLoadingState());
+    final result = getMessagesUseCase.call(userId: userId);
+      result.fold((l) =>emit(GetAllMessagesErrorState()),
+       (r) {
+        r.listen((messages) { 
+          this.messages=messages;
+          emit(GetAllMessagesSuccessState(messages));
+        });
+       });
+  }
+
+   Future<void> getAllUsers() async {
+    emit(GetUserLoadingState());
+     final result=await getAllUsersUseCase.call();
+     result.fold((l) => emit(GetUserErrorState()),
+          (allUsers) { 
+            GetUserSuccessState.setAllUsers(allUsers);
+            emit(GetUserSuccessState());});    
+  }
 }
--- lib/CleanArch/features/chat/presentation/manager/cubit/chat_state.dart
+++ lib/CleanArch/features/chat/presentation/manager/cubit/chat_state.dart
@@ -1,6 +1,35 @@
+// presentation/cubit/chat_state.dart
+
+
 part of 'chat_cubit.dart';
 
 @immutable
-sealed class ChatState {}
+abstract class ChatState {}
 
-final class ChatInitial extends ChatState {}
+class ChatInitial extends ChatState {}
+
+class AddChatLoadingState extends ChatState {}
+
+class AddChatSuccessState extends ChatState {}
+
+class AddChatErrorState extends ChatState {}
+
+class GetAllMessagesLoadingState extends ChatState {}
+
+class GetAllMessagesSuccessState extends ChatState {
+  final List<MessagesModel> messages;
+  GetAllMessagesSuccessState(this.messages);
+}
+
+class GetAllMessagesErrorState extends ChatState {}
+
+class GetUserLoadingState extends ChatState {}
+
+class GetUserSuccessState extends ChatState {
+  static List<UserModel>allUsers=[];
+  static void setAllUsers(List<UserModel>users){
+    allUsers=users;
+  }
+}
+
+class GetUserErrorState extends ChatState {}
