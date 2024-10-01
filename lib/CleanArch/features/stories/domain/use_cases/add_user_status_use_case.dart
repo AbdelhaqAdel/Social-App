d38b8b04288536db28dcb9f505597c72982ac411
abdelhaq <147981005+AbdelhaqAdel@users.noreply.github.com>
2024-10-01 23:28:03 +0300

    add status domain layer

--- lib/CleanArch/features/stories/domain/use_cases/add_user_status_use_case.dart
+++ lib/CleanArch/features/stories/domain/use_cases/add_user_status_use_case.dart
@@ -1,15 +1,14 @@
 import 'package:dartz/dartz.dart';
-import 'package:newapp/CleanArch/core/usecases/use_case.dart';
+import 'package:flutter/material.dart';
 import 'package:newapp/CleanArch/core/utils/error_handliing.dart';
 import 'package:newapp/CleanArch/features/stories/domain/repositories/status_repo.dart';
 
-class AddUserStatusUseCase extends UseCase<void,String,Failure>{
+class AddUserStatusUseCase {
   final StatusRepository repository;
 
   AddUserStatusUseCase(this.repository);
 
-  @override
-  Future<Either<Failure,void>> call([String?param]) {
-    return repository.addUserStatus(statusText:param!);
+  Future<Either<Failure,void>> call({required String text,required Color statusColor}) {
+    return repository.addUserStatus(statusText:text,statusColor: statusColor);
   }
 }
--- lib/CleanArch/features/stories/domain/use_cases/get_user_status_use_case.dart
+++ lib/CleanArch/features/stories/domain/use_cases/get_user_status_use_case.dart
@@ -3,12 +3,12 @@ import 'package:newapp/CleanArch/core/utils/error_handliing.dart';
 import 'package:newapp/CleanArch/features/stories/data/models/status_model.dart';
 import 'package:newapp/CleanArch/features/stories/domain/repositories/status_repo.dart';
 
-class GetAllStatusUseCase {
+class GetUserStatusUseCase {
   final StatusRepository repository;
 
-  GetAllStatusUseCase(this.repository);
+  GetUserStatusUseCase(this.repository);
 
-  Either<Failure,Stream<List<StatusModel>>> call() {
-    return repository.getAllStatus();
+  Either<Failure,Stream<List<StatusModel>>> call({required String userId}) {
+    return repository.getUserStatus(userId: userId);
   }
 }
