642c2005cadccf0c020b3d347e13c90d42f256ef
abdelhaq <147981005+AbdelhaqAdel@users.noreply.github.com>
2024-10-03 17:59:56 +0300

    profile domain layer

--- lib/CleanArch/features/profile/domain/repositories/profile_repo.dart
+++ lib/CleanArch/features/profile/domain/repositories/profile_repo.dart
@@ -4,4 +4,8 @@ import 'package:newapp/CleanArch/features/profile/data/models/user_model.dart';
 
 abstract class IProfileRepo {
   Future<Either<Failure, UserModel>> getUserProfile({required String uid});
+    // Future<Either<String,void>>editProfile({required String postText});
+  
+  
+  Future<Either<String, void>> uploadProfileImage({required String pickedImage});
 }
