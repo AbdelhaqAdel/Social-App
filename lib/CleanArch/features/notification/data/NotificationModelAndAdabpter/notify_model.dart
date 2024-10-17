6e6437c4a985c6b912ae5ec2181554bd8635c975
abdelhaq <147981005+AbdelhaqAdel@users.noreply.github.com>
2024-10-17 17:27:29 +0300

    add notification data layer

---
+++ lib/CleanArch/features/notification/data/NotificationModelAndAdabpter/notify_model.dart
@@ -0,0 +1,7 @@
+class NotifyModel{
+  int?hiveIndex;
+  String? notifyTitle;
+  String? notifyBody;
+  String? notifyTime;
+  NotifyModel({this.hiveIndex,this.notifyTitle,this.notifyBody,this.notifyTime});
+}
